1205baea7561188eb3843183917d4478739189cc
Scott McGowan <evilmonkeyinc@gmail.com>
2021-11-18 22:31:55 +0000

    feat: add string array flag type
    add custom flag type to support string arrays
    
    supports comma-separated lists and multiple flags

---
+++ shell/flagtypes.go
@@ -0,0 +1,28 @@
+package shell
+
+import "strings"
+
+// StringArrayFlag is a Value implementation that
+// adds string array support to the FlagSet
+// StringArrayFlag supports multiple uses of the same flag,
+// as well as comma seperated lists.
+type StringArrayFlag []string
+
+// String retruns the string array as a comma seperated string.
+func (flag *StringArrayFlag) String() string {
+	return strings.Join(*flag, ",")
+}
+
+// Set adds the supplied string to the string array.
+// If the string contains comma seperators, the string will be split
+// before being added to the array.
+func (flag *StringArrayFlag) Set(value string) error {
+	split := strings.Split(value, ",")
+	*flag = append(*flag, split...)
+	return nil
+}
+
+// Get returns the string array value.
+func (flag *StringArrayFlag) Get() interface{} {
+	return *flag
+}
---
+++ shell/flagtypes_test.go
@@ -0,0 +1,76 @@
+package shell
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_StringArrayFlag(t *testing.T) {
+
+	type expected struct {
+		array []string
+		str   string
+	}
+
+	tests := []struct {
+		name     string
+		input    []string
+		expected expected
+	}{
+		{
+			name:  "one",
+			input: []string{"-array", "one"},
+			expected: expected{
+				array: []string{"one"},
+				str:   "one",
+			},
+		},
+		{
+			name:  "two",
+			input: []string{"-array", "one", "-array", "two"},
+			expected: expected{
+				array: []string{"one", "two"},
+				str:   "one,two",
+			},
+		},
+		{
+			name:  "three",
+			input: []string{"-array", "one", "-array", "two", "-array", "three"},
+			expected: expected{
+				array: []string{"one", "two", "three"},
+				str:   "one,two,three",
+			},
+		},
+		{
+			name:  "comma-seperated",
+			input: []string{"-array", "one,two,three"},
+			expected: expected{
+				array: []string{"one", "two", "three"},
+				str:   "one,two,three",
+			},
+		},
+		{
+			name:  "combo",
+			input: []string{"-array", "one", "-array", "two,three"},
+			expected: expected{
+				array: []string{"one", "two", "three"},
+				str:   "one,two,three",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			flagSet := flag.NewFlagSet("", 0)
+
+			value := &StringArrayFlag{}
+			flagSet.Var(value, "array", "an array")
+			flagSet.Parse(test.input)
+
+			assert.EqualValues(t, test.expected.array, value.Get())
+			assert.EqualValues(t, test.expected.str, value.String())
+		})
+	}
+}
